1e3a070af2c39c4621f2536f3e992edda50fedb3
Dhiqal34rpl <muhammad_karna_34rpl@student.smktelkom-mlg.sch.id>
2025-08-06 09:44:07 +0700

    Add files via upload

---
+++ Coding BU Aulia/datasiswa.java
@@ -0,0 +1,17 @@
+public class datasiswa {
+    public static void main(String[] args) {
+        String nama = "Dhiqal";
+        String Kelas = "X RPL 7";
+        int umur = 15;
+        double beratBadan =40.50;
+        boolean status=true;
+
+        System.out.println("nama: "  + nama);
+        System.out.println("kelas: " + Kelas);
+        System.out.println("umur: " + umur);
+        System.out.println("beratBadan: " + beratBadan);
+        System.out.println("status: " + status);
+    }
+
+
+}
